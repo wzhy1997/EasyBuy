253e64146f60ddac0268fbdeb2ec7db68afcd644
beforeme <475092089@qq.com>
2023-07-01 23:16:02 +0800

    "项目搭建以及静态资源配置"

---
+++ src/main/java/com/wzhy/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.wzhy.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+@Slf4j
+@Configuration
+public class WebMvcConfig  extends WebMvcConfigurationSupport {
+//    静态资源访问设置
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+            log.info("开始静态资源映射");
+            registry.addResourceHandler("/backend/**")
+                    .addResourceLocations("classpath:/backend/");
+            registry.addResourceHandler("/front/**")
+                    .addResourceLocations("classpath:/front/");
+
+            }
+}
